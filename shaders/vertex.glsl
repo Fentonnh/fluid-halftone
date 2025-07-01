277f18f0f31a5c29b73910623dc0d0703547e155
Fentonnh <fentonnh@gmail.com>
2025-07-01 16:21:23 -0400

    Update 4 vertex.glsl

--- shaders/vertex.glsl
+++ shaders/vertex.glsl
@@ -1,4 +1,8 @@
 attribute vec3 aPosition;
+uniform vec2 resolution;
+
 void main() {
-  gl_Position = vec4(aPosition, 1.0);
+  vec2 clip = (aPosition.xy / resolution) * 2.0;
+  clip.y *= -1.0;
+  gl_Position = vec4(clip, aPosition.z, 1.0);
 }
